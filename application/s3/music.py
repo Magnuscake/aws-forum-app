4a7cc5cd00c021d15a224dfc4bc0ff5aff4db6a4
Magnuscake <saneyar.k@gmail.com>
2021-05-03 16:15:14 +1000

    Cleaned up error logging

--- application/s3/music.py
+++ application/s3/music.py
@@ -10,9 +10,9 @@ s3_client = boto3.client('s3')
 BUCKET_NAME = 's3798420-artist-images'
 
 def upload_artist_images(artist_img_urls):
-
     for url in artist_img_urls:
         key = url.split('/')[-1]
+
         if not key_exists_in_bucket(key):
             upload_image_from_url(url)
 
@@ -28,8 +28,7 @@ def key_exists_in_bucket(key):
     try:
         s3.Object(BUCKET_NAME, key).load()
 
-    except ClientError as ce:
-        logging.error(ce)
+    except ClientError:
         return False
 
     return True
